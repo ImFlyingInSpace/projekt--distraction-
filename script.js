a9c5ac0b4c0fe9f2ad46b122c45f25b6a5723bf2
Nini <kunst.n42@gmail.com>
2026-08-04 15:06:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const today = new Date();
+let month = today.getMonth();
+const scheduleMonth = month +1;
+let year = today.getFullYear();
+console.log(scheduleMonth);
+function getDaysInMonth(scheduleMonth, year){
+    return new Date(year, scheduleMonth +1, 0).getDate();
+}
+console.log(getDaysInMonth(scheduleMonth, year));
+
+function writeCalendar(scheduleMonth,year){
+    let days = getDaysInMonth(scheduleMonth, year);
+    const calendar = document.getElementById("calendar");
+    for (let day = 1; day <= days; day++){
+        calendar.innerHTML += `
+            <tr>
+                <td>${day}</td>
+                <td>
+                    <select>
+                        <option class="day">Celý den</option>
+                        <option class="morning">Ráno</option>
+                        <option class="noon">Odpoledne</option>
+                        <option class="night">Noc</option>
+                    </select>
+                </td>
+            </tr>
+        `;
+    }
+}
+writeCalendar(scheduleMonth,year);
