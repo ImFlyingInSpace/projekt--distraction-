d46e37ce63ca836b354b74604f3622aaf3158823
Nini <kunst.n42@gmail.com>
2026-08-04 20:50:39 +0200

    idk

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
+//konstanty a premety
+
 const today = new Date();
 let month = today.getMonth();
-const scheduleMonth = month +1;
+let scheduleMonth = month +1;
 let year = today.getFullYear();
-console.log(scheduleMonth);
-function getDaysInMonth(scheduleMonth, year){
-    return new Date(year, scheduleMonth +1, 0).getDate();
+let scheduleYear = year;
+if(scheduleMonth>11){
+    scheduleMonth = 0;
+    scheduleYear++;
 }
-console.log(getDaysInMonth(scheduleMonth, year));
+console.log(scheduleMonth);
+const months = ["Leden", "Únor", "Březen", "Duben", "Květen", "Červen", "Červenec", "Srpen", "Září", "Říjen","Listopad","Prosinec"];
 
-function writeCalendar(scheduleMonth,year){
-    let days = getDaysInMonth(scheduleMonth, year);
+
+
+
+
+
+
+
+
+
+//funkce
+
+function getDaysInMonth(scheduleMonth, scheduleYear){
+    return new Date(scheduleYear, scheduleMonth +1, 0).getDate();
+}
+console.log(getDaysInMonth(scheduleMonth, scheduleYear));
+
+function writeCalendar(scheduleMonth,scheduleYear){
+    let days = getDaysInMonth(scheduleMonth, scheduleYear);
     const calendar = document.getElementById("calendar");
+    const monthYear = document.getElementById("month-year");
+    monthYear.innerHTML = `
+        <h3>${months[scheduleMonth]} ${scheduleYear}</h3>
+    `;
     for (let day = 1; day <= days; day++){
         calendar.innerHTML += `
-            <tr>
-                <td>${day}.</td>
-                <td>
-                    <select>
-                        <option class="day">Celý den</option>
-                        <option class="morning">Ráno</option>
-                        <option class="noon">Odpoledne</option>
-                        <option class="night">Noc</option>
-                    </select>
-                </td>
-            </tr>
+            <div class="day-box">
+                <div class="day-number">${day}.</div>
+                <select>
+                    <option value="" selected disabled>Dostupnost</option>
+                    <option value="X">X</option>
+                    <option value="CD">Celý den</option>
+                    <option value="R">Ráno 10-14</option>
+                    <option value="O">Odpoledne 14-19</option>
+                    <option value="P">Pozdní 15-19</option>
+                </select>
+            </div>
         `;
     }
 }
-writeCalendar(scheduleMonth,year);
+
+
+
+
+//volani
+
+writeCalendar(scheduleMonth,scheduleYear);
